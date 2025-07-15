5cfb688941cb0e7374ef65be82e9cde50d748ac4
Weriton Luis Petreca <eulcf@hotmail.com>
2025-07-15 13:51:02 -0300

    feat: new repositiry classes added

---
+++ src/main/java/com/petreca/exceptions/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package com.petreca.exceptions;
+
+public class AccountNotFoundException extends RuntimeException {
+  public AccountNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/petreca/repository/AccountRepository.java
@@ -0,0 +1,4 @@
+package com.petreca.repository;
+
+public class AccountRepository {
+}
